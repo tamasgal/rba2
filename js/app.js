a27ea638d8f43094a10ae38273406814498f65fb
Tamas Gal <himself@tamasgal.com>
2017-07-28 00:38:12 +0200

    Refactoring and add km3srv connection form

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
     // var km3srv = new KM3srv.Connection("ws://maroff1.in2p3.fr:8088/test");
-    var km3srv = new KM3srv.Connection("ws://localhost:8088/test");
+    var km3srv = new KM3srv.Connection("localhost", 8088);
+    //var km3srv = new KM3srv.Connection("ws://evalu176.ific.uv.es:8088/test");
 
     var clipboard = new Clipboard('#tokenLabel');
 
--- js/km3srv.js
+++ js/km3srv.js
@@ -1,49 +1,72 @@
 var KM3srv = (function() {
     var self = {};
 
-    self.Connection = function(url) {
-        var ws = new WebSocket(url);
+    self.ws = null;
+
+    var onopen = function() {
+        'use strict';
+        self.ws.send("hello");
+    };
+
+    var onmessage = function (raw_message) {
+        'use strict';
+        var message = JSON.parse(raw_message.data);
+        console.log(message);
+        if(message.kind == "info") {
+            RBA.log( message.data );
+        }
+        if(message.kind == "status") {
+            var statusLbl = $("#statusLabel");
+            statusLbl.removeClass("label-success label-warning label-danger");
+            if(message.data == "busy") {
+                statusLbl.addClass("label-warning");
+            }
+            if(message.data == "ready") {
+                statusLbl.addClass("label-success");
+            }
+        }
+        if(message.kind == "event") {
+            draw_event(message.data);
+        }
+        if(message.kind == "token") {
+            var token = message.data;
+            $("#tokenLabel").text(token);
+            $("#tokenLabel").data('clipboard-text', token);
+            RBA.log( "Your token for remote display is " + token + "." );
+        }
+    };
+
+    self.connect = function(hostname, port) {
+        var url = "ws://" + hostname + ":" + port + "/test";
+        self.ws = new WebSocket(url);
+        RBA.log( "Connecting to " + hostname + ":" + port);
+        self.ws.onopen = onopen;
+        self.ws.onmessage = onmessage;
+    }
+
+    self.Connection = function(hostname, port) {
+
+        self.connect(hostname, port);
 
         $("#loadEventBtn").click(function(){
             var det_id = $("#detectorInput").val();
             var run_id = $("#runInput").val();
             var event_id = $("#eventInput").val();
-            ws.send("event/" + det_id + "/" + run_id + "/" + event_id);
+            self.ws.send("event/" + det_id + "/" + run_id + "/" + event_id);
         });
 
 
-        ws.onopen = function() {
-            'use strict';
-            ws.send("hello");
-        };
+        $("#connectToKm3srvBtn").click(function(){
+            var hostname = $("#km3srvHostInput").val();
+            var port = $("#km3srvPortInput").val();
+            console.log(hostname);
+            console.log(port);
+            self.ws.close();
+            self.connect(hostname, port);
+        });
 
-        ws.onmessage = function (raw_message) {
-            'use strict';
-            var message = JSON.parse(raw_message.data);
-            console.log(message);
-            if(message.kind == "info") {
-                RBA.log( message.data );
-            }
-            if(message.kind == "status") {
-                var statusLbl = $("#statusLabel");
-                statusLbl.removeClass("label-success label-warning label-danger");
-                if(message.data == "busy") {
-                    statusLbl.addClass("label-warning");
-                }
-                if(message.data == "ready") {
-                    statusLbl.addClass("label-success");
-                }
-            }
-            if(message.kind == "event") {
-                draw_event(message.data);
-            }
-            if(message.kind == "token") {
-                var token = message.data;
-                $("#tokenLabel").text(token);
-                $("#tokenLabel").data('clipboard-text', token);
-                RBA.log( "Your token for remote display is " + token + "." );
-            }
-        };
+
+
     };
 
     return self;
